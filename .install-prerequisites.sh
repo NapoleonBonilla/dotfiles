5cfdf0ce437e60d6967717cd6c1f5843ad3527d6
NapoleonBonilla <nbonilla@crexendo.com>
2025-08-28 07:42:58 -0600

    Update .install-prerequisites.sh

--- .install-prerequisites.sh
+++ .install-prerequisites.sh
@@ -5,11 +5,9 @@ if !type keeper &>/dev/null || !type bw &>/dev/null; then
     case "$(uname -s)" in
     Darwin)
         brew install keeper-commander
-        brew install bitwarden-cli
         ;;
     Linux)
         nix-env -iA nixpkgs.keeper-commander
-        nix-env -iA nixpkgs.bitwarden-cli
         ;;
     *)
         echo "unsupported OS"
